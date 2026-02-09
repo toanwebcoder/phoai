df4392c8a27c60c99fb1aaeca1f4467052155d8f
toandev <toanwwordpresstheme@gmail.com>
2026-02-09 14:16:48 +0700

    fix: light dark layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ colorScheme: 'light' }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
